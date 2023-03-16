7ed15e1185eb2ffe5d4490e084e44aec85164641
kamonphop2001 <kamonphop.pr@hotmail.com>
2023-03-16 11:22:58 +0700

    change repo

---
+++ Config/Config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    development: {
+      username: 'root',
+      password: 'root',
+      database: 'Platform',
+      host: 'localhost',
+      dialect: 'mysql',
+      port:'3306',
+    },
+    test: {
+      username: 'root',
+      password: 'root',
+      database: 'Platform',
+      host: 'localhost',
+      dialect: 'mysql',
+      port:'3306',
+    },
+    production: {
+      username: 'root',
+      password: 'root',
+      database: 'Platform',
+      host: 'localhost',
+      dialect: 'mysql',
+      port:'3306',
+    },
+  };
+  
